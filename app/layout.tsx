766770114b049b30d19032ef093cbb592ae369e7
Tyler Trebitowski <tyler@investii.com>
2023-11-10 15:06:25 -0600

    Grainy background

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,21 @@ export default function RootLayout({
           quicksand.className
         }
       >
+        <svg
+          className="pointer-events-none fixed isolate z-50 opacity-70 mix-blend-soft-light"
+          width="100%"
+          height="100%"
+        >
+          <filter id="bg-grain">
+            <feTurbulence
+              type="fractalNoise"
+              baseFrequency="0.83"
+              numOctaves="3"
+              stitchTiles="stitch"
+            />
+          </filter>
+          <rect width="100%" height="100%" filter="url(#bg-grain)"></rect>
+        </svg>
         {children}
       </body>
     </html>
